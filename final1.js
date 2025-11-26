aa73e125f27828b49f582be100318ca5f22a7741
analuizast1212-crypto <analuizast1212@gmail.com>
2025-11-26 15:09:34 -0300

    final2

---
+++ final1.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync') ({sigint: true}); 
+
+let numeros =[ 2,4,6,8]
+let soma=0
+for (let i=0; i< numeros.length, i++)
+   {
+    soma= soma+numeros[i];
+}
+console.log("O resultado é:",numeros) 
---
+++ final2.js
@@ -0,0 +1,6 @@
+
+const valores = [3,5,7,12,18]
+let maior = valores.filter((valor)=>
+{return valor >10})
+console.log(maior)
+
