a33de49917fd8e343db80626e76a0fc935994640
analuizast1212-crypto <analuizast1212@gmail.com>
2025-11-26 17:12:50 -0300

    final17

---
+++ final17.js
@@ -0,0 +1,8 @@
+const array = [1, 2, 3, 4];
+
+console.log(`Array original: ${array}`);
+
+// Invertendo o array
+const arrayInvertido = array.reverse();
+
+console.log(`Array invertido: ${arrayInvertido}`);
