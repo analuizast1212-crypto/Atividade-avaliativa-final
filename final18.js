3f9d02d5085e8f3d113b29f1e7fc5e089c063f72
analuizast1212-crypto <analuizast1212@gmail.com>
2025-11-26 17:15:48 -0300

    final18

---
+++ final18.js
@@ -0,0 +1,9 @@
+const senha = "abc123"; 123
+
+if (senha.length < 6) {
+  console.log(`Senha: "${senha}"`);
+  console.log("Força da senha: FRACA");
+} else {
+  console.log(`Senha: "${senha}"`);
+  console.log("Força da senha: OK");
+}
